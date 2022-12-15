ebd2bb8be392e87db4915c851108d82e2a0500cb
FWink <12393262+FWink@users.noreply.github.com>
2022-12-15 15:50:48 +0100

    Added missing import line to ActivityRecognitionLocationProvider

--- android/common/src/main/java/com/marianhello/bgloc/provider/ActivityRecognitionLocationProvider.java
+++ android/common/src/main/java/com/marianhello/bgloc/provider/ActivityRecognitionLocationProvider.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.util.Log;
